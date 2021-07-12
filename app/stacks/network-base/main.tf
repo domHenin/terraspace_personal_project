be583d04889d359724f44e105fe7623ebe0c0eb9
dominic <domhenin.517@gmail.com>
2021-07-12 13:18:43 -0400

    was able to successfully run 'terraspace all up' command

--- app/stacks/network-base/main.tf
+++ app/stacks/network-base/main.tf
@@ -8,7 +8,32 @@ resource "aws_vpc" "vpc" {
 
 resource "aws_subnet" "subnet" {
   cidr_block = var.subnet_cidr
-  vpc_id = aws_vpc.vpc.id
+  vpc_id     = aws_vpc.vpc.id
+}
+
+resource "aws_security_group" "container_sg" {
+  name        = "container_sg"
+  description = "allow inbound traffic"
+
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port = var.sg_port
+    protocol  = "tcp"
+    to_port   = var.sg_port
+    cidr_blocks = var.allow_inbound_cidrs
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.sg_tag
+  }
 }
 
 //resource "aws_network_interface" "test" {
--- app/stacks/network-base/variables.tf
+++ app/stacks/network-base/variables.tf
@@ -12,3 +12,18 @@ variable "subnet_cidr" {
   description = "used cidr block for subnet"
   type        = string
 }
+
+variable "sg_port" {
+  description = "port used for traffic"
+  type        = number
+}
+
+variable "allow_inbound_cidrs" {
+  description = "allowing inbound with cidrs range"
+  type        = list(string)
+}
+
+variable "sg_tag" {
+  description = "allow tls"
+  type        = string
+}
